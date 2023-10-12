000438f8b8f28f5f67cb16a7a4fb06cdfa37e6ab
yong1le <ygle.he@gmail.com>
2023-10-12 01:48:24 -0400

    update contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
+// Finding a Local Church
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div>
+      <div className="page-width section bg-beige">
+        <h1 className="responsive-title">Join Us!</h1>
+      </div>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
