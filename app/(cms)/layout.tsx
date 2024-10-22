5ee1d5aecd1c47718d9b83e3824ac5b1d5b15442
Yong Le He <ygle.he@gmail.com>
2024-10-21 23:06:09 -0400

    feat: add outstatic cms

---
+++ app/(cms)/layout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function CMSRootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
