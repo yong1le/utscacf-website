1f9176806cebbad4d014f82e156bf7064756ea1b
Yong Le He <ygle.he@gmail.com>
2024-10-23 22:00:04 -0400

    fix: event type placeholder

--- components/shared/event/EventCarousel.tsx
+++ components/shared/event/EventCarousel.tsx
@@ -8,20 +8,20 @@ import {
 import Event from "@/components/shared/event/Event";
 
 const EventCarousel = async () => {
-  const events = [];
+  const events: string[] = [];
 
   return (
     <Carousel>
       <CarouselContent className="flex flex-row">
         {events.map((e, i) => (
           <CarouselItem key={i} className="md:basis-1/2 xl:basis-1/3">
-            <Event
-              key={i}
-              name={e.entry.name}
-              image={e.entry.image}
-              link={e.entry.link}
-              description={e.entry.description}
-            />
+            {/* <Event */}
+            {/*   key={i} */}
+            {/*   name={e.entry.name} */}
+            {/*   image={e.entry.image} */}
+            {/*   link={e.entry.link} */}
+            {/*   description={e.entry.description} */}
+            {/* /> */}
           </CarouselItem>
         ))}
       </CarouselContent>
