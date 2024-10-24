c037bc1e40fc123d5c4db885d8c56a276950ffb1
Yong Le He <ygle.he@gmail.com>
2024-10-23 21:55:36 -0400

    fix: remove unused component

--- components/shared/event/Event.tsx
+++ components/shared/event/Event.tsx
@@ -8,23 +8,24 @@ import {
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
-const Event = ({ name, image, link, description }) => {
-  return (
-    <Card className="w-full overflow-hidden">
-      <div className="relative h-[200px] w-[100%]">
-        <Image src={image || "/ministry.png"} alt="Event Image" />
-      </div>
-      <CardContent className="flex flex-col items-start gap-3 pt-6">
-        <CardTitle>{name}</CardTitle>
-        <CardDescription>{description}</CardDescription>
-        <Button asChild className="self-end">
-          <Link href={link} target="_blank">
-            Learn More
-          </Link>
-        </Button>
-      </CardContent>
-    </Card>
-  );
-};
+// const Event = ({ name, image, link, description }) => {
+//   return (
+//     <Card className="w-full overflow-hidden">
+//       <div className="relative h-[200px] w-[100%]">
+//         <Image src={image || "/ministry.png"} alt="Event Image" />
+//       </div>
+//       <CardContent className="flex flex-col items-start gap-3 pt-6">
+//         <CardTitle>{name}</CardTitle>
+//         <CardDescription>{description}</CardDescription>
+//         <Button asChild className="self-end">
+//           <Link href={link} target="_blank">
+//             Learn More
+//           </Link>
+//         </Button>
+//       </CardContent>
+//     </Card>
+//   );
+// };
 
+const Event = () => {};
 export default Event;
